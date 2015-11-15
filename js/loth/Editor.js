50ef3d829a84e2599ae4c68be39343b85655124f
Pedro Martins <pedro.m4rtins@gmail.com>
2015-11-15 20:35:26 -0300

    correct code indentation
    and delete a big chunk of commented code

--- js/loth/Editor.js
+++ js/loth/Editor.js
@@ -13,7 +13,7 @@ var Editor = function (Pos) {
 	var type = "color";
 	var open = false;
 
-    var container = doc.createElement( 'div' );
+  var container = doc.createElement( 'div' );
 	container.style.cssText = unselect+'position:absolute; margin:0; padding:0; top:0px; left:50%; color:#CCCCCC; width:50%; height:100%; font-size:12px; font-family:Consolas; pointer-events:none; display:none; background: linear-gradient(45deg, #1d1f20, #2f3031);';
 	container.id = 'Editor';
 
@@ -53,7 +53,6 @@ var Editor = function (Pos) {
 		"l-37.252,37.253c29.758,29.757,70.867,48.162,116.273,48.162c90.814,0,164.436-73.644,164.436-164.459c0-0.007,0,0.008,0,0H462z'/></svg>"
 	].join("\n");
 
-
 	var colors = ['#303030', '#b10dc9', '#0074d9', '#ff851b'];
 	var buttonActif = 'position:relative; display:inline-block; cursor:pointer; pointer-events:auto;';
 	var bstyle =unselect+ ' font-size:14px; margin-right:4px; -webkit-border-radius:40px; border-radius:40px;  border:2px solid #343434; background:'+colors[0]+'; height:19px; padding:2px 2px; text-align:center;';
@@ -69,8 +68,8 @@ var Editor = function (Pos) {
 	decoFrame.style.cssText =unselect+'top:10px; left:130px; position:absolute; display:block; width:calc(100% - 120px); height:60px; overflow:hidden; padding:0;';
 	container.appendChild( decoFrame );
 
-    // RUN BUTTON
-    var bRun = doc.createElement( 'div' );
+  // RUN BUTTON
+  var bRun = doc.createElement( 'div' );
 	bRun.id = 'Editor-Run';
 	bRun.style.cssText = bstyle + buttonActif + 'top:10px; left:20px; position:absolute; width:46px; height:46px;';
 	var rvalue = 0;
@@ -83,26 +82,19 @@ var Editor = function (Pos) {
 	icRun.style.cssText = "position:absolute; width:46px; height:46px; pointer-events:none;";
 	icRun.innerHTML = icon_update; 
 	container.appendChild( bRun );
-	bRun.appendChild(icColor);
+	
+  bRun.appendChild(icColor);
 	bRun.appendChild(icRun);
 	bRun.addEventListener( 'mousedown', function ( event ) { event.preventDefault(); update(); icColor.style.backgroundColor = 'rgba(0,116,217,0.7)'; }, false );
-	bRun.addEventListener( 'mouseover', function ( event ) { event.preventDefault();  icColor.style.backgroundColor = 'rgba(0,116,217,0.1)'; updateTimer = setInterval(rotateUpdate, 10, icRun); doc.getElementById("icon_update").setAttribute('fill','#'+selColor);}, false );
-    bRun.addEventListener( 'mouseout', function ( event ) { event.preventDefault(); icColor.style.backgroundColor = 'rgba(0,0,0,0)'; clearInterval(updateTimer); doc.getElementById("icon_update").setAttribute('fill','#'+outColor);}, false );
+	bRun.addEventListener( 'mouseover', function ( event ) { event.preventDefault(); icColor.style.backgroundColor = 'rgba(0,116,217,0.1)'; updateTimer = setInterval(rotateUpdate, 10, icRun); doc.getElementById("icon_update").setAttribute('fill','#'+selColor);}, false );
+  bRun.addEventListener( 'mouseout', function ( event ) { event.preventDefault(); icColor.style.backgroundColor = 'rgba(0,0,0,0)'; clearInterval(updateTimer); doc.getElementById("icon_update").setAttribute('fill','#'+outColor);}, false );
 
-    var rotateUpdate = function (dom) {
-    	rvalue -= 5;
+  var rotateUpdate = function (dom) {
+    rvalue -= 5;
 		dom.style.webkitTransform = 'rotate('+rvalue+'deg)';
 		dom.style.oTransform = 'rotate('+rvalue+'deg)';
 		dom.style.transform = 'rotate('+rvalue+'deg)';
 	}
-	/*var bRun = doc.createElement( 'div' );
-	bRun.id = 'Editor-Run';
-	bRun.style.cssText =bstyle+buttonActif+'top:10px; left:10px; position:absolute; width:100px; height:30px; padding-top:12px;';
-	bRun.textContent = "RUN SCRIPT";
-	container.appendChild( bRun );
-	bRun.addEventListener( 'mousedown', function ( event ) { event.preventDefault(); update(); this.style.backgroundColor = colors[3]; }, false );
-	bRun.addEventListener( 'mouseover', function ( event ) { event.preventDefault();  this.style.backgroundColor = colors[2]; }, false );
-    bRun.addEventListener( 'mouseout', function ( event ) { event.preventDefault(); this.style.backgroundColor = colors[0]; }, false );*/
 
     // MENU DEMO
 	for(var i=0;i!==maxDemo;i++){
@@ -122,7 +114,6 @@ var Editor = function (Pos) {
 	}
 
 
-
 	// MAIN EDITOR
 	var MainEditor = doc.createElement( 'iframe' );
 	MainEditor.id = 'mEditor';
@@ -130,7 +121,6 @@ var Editor = function (Pos) {
 	MainEditor.src = "demos/mainEditor.html";
 	MainEditor.style.cssText = unselect+"  top:70px; bottom:0px; left:0; margin:0; padding:0; position:absolute; height:calc(100% - 74px); width:calc(100% - 4px); display:block; pointer-events:auto; border:none;"
 	container.appendChild( MainEditor );
-
 
 	var importScript = function(name){
 		MainEditor.contentWindow.setBase(Editor);
@@ -146,7 +136,7 @@ var Editor = function (Pos) {
 			else bbMenu[i].style.backgroundColor = colors[0];
 		}
 	}
-
+  
 	var update = function (){
 		var head = doc.getElementsByTagName('head')[0];
 		nscript = doc.createElement("script");
